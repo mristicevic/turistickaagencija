836e5b8dcd6d3148d62274af9521c0b5b8a4b87f
Mirjana Risticevic <mristicevic@gmail.com>
2022-04-16 01:18:01 +0200

    vue alert

--- resources/views/user/alltrips.blade.php
+++ resources/views/user/alltrips.blade.php
@@ -1,47 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-
-    <title>Sixteen Clothing Products</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<!--
-
-TemplateMo 546 Sixteen Clothing
-
-https://templatemo.com/tm-546-sixteen-clothing
-
--->
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-sixteen.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-
-  </head>
-
-  <body>
-    <style>
-   
-    </style>
+@extends('user.frontend')
+@section('content')
     <!-- ***** Preloader Start ***** -->
-    <div id="preloader">
-        <div class="jumper">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>  
-    <!-- ***** Preloader End ***** -->
-    @include('user.header')
     
     <!-- Page Content -->
     <div class="page-heading products-heading header-text">
@@ -88,13 +47,8 @@ https://templatemo.com/tm-546-sixteen-clothing
                 <h6>{{$product->price}} €</h6>
                 <p>{{$product->description}}</p>
 
-                <form action="{{url('addcart',$product->id)}}" method="POST">
-                  @csrf
-                  <input type="number" value="1" min="1" class="form-control" style="width:100px" name="quantity">
-                  <br>
-                  <input class="btn btn-primary" type ="submit" value="Add cart" style="width:150px">
-                </form>
-
+                
+                <add-to-cart-button trip-id="{{$product->id}}" user-id="{{auth()->user()->id ?? 0}}"/>
                 
               </div>
             </div>
@@ -108,46 +62,7 @@ https://templatemo.com/tm-546-sixteen-clothing
     
 
     
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="inner-content">
-              <p>Copyright &copy; 2020 Travel Pro, Ltd.
-            
-            - Design: <a rel="nofollow noopener" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    
 
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-
-    <!-- Additional Scripts -->
-    <script src="assets/js/custom.js"></script>
-    <script src="assets/js/owl.js"></script>
-    <script src="assets/js/slick.js"></script>
-    <script src="assets/js/isotope.js"></script>
-    <script src="assets/js/accordions.js"></script>
-
-
-    <script language = "text/Javascript"> 
-      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
-      function clearField(t){                   //declaring the array outside of the
-      if(! cleared[t.id]){                      // function makes it static and global
-          cleared[t.id] = 1;  // you could use true and false, but that's more typing
-          t.value='';         // with more chance of typos
-          t.style.color='#fff';
-          }
-      }
-    </script>
-
-
-  </body>
-
-</html>
+    @endsection
