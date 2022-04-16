824e0f33d279f750f66b7f504f30db4e59b61d2a
Mirjana Risticevic <mristicevic@gmail.com>
2022-04-16 16:04:02 +0200

    vue alert

---
+++ resources/js/components/AddToCart.vue
@@ -0,0 +1,27 @@
+<template>
+    <div >
+        <button class="btn btn-danger text-center " v-on:click.prevent="addTripToCart">Add to cart
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            
+    },
+    props:['tripId','userId']   ,
+    methods:{
+        addTripToCart(){
+            if(this.userId == 0){
+                this.$toastr.e('You need to login!');
+                return;
+            }
+            
+        }
+    }  , 
+       mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
